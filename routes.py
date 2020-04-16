65aa509e4752ff44197d1b9bba13f6e9845ddd3b
Sean <seanmooney@live.co.uk>
2020-04-16 23:57:40 +0100

    Final commit - log messages updated

--- routes.py
+++ routes.py
@@ -19,26 +19,26 @@ app.secret_key = app.config['SECRET_KEY']
 @app.route("/")
 def index():
     this_route = url_for('.index')
-    app.logger.info("Logging a test message from {}".format(this_route))
+    app.logger.info("GET {}".format(this_route))
     return render_template("index.html")
 
 @app.route("/about")
 def about():
     this_route = url_for('.about')
-    app.logger.info("Logging a test message from {}".format(this_route))
+    app.logger.info("GET {}".format(this_route))
     return render_template("about.html")
 
 @app.route("/contact")
 def contact():
     this_route = url_for('.contact')
-    app.logger.info("Logging a test message from {}".format(this_route))
+    app.logger.info("GET {}".format(this_route))
     return render_template("contact.html")
 
 @app.route("/map", methods=["GET", "POST"])
 def map():
     this_route = url_for('.map')
     if 'email' not in session:
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Can't access {} without session id".format(this_route))
         return redirect(url_for('login'))
 
     form = AddressForm()
@@ -50,7 +50,7 @@ def map():
 
     if request.method == 'POST':
         if form.validate() == False:
-            app.logger.info("Logging a test message from {}".format(this_route))
+            app.logger.info("Map request invalid {}".format(this_route))
             return render_template('map.html', form=form)
         else:
             # get the address
@@ -59,12 +59,12 @@ def map():
             p = Place()
             my_coordinates = p.address_to_latlng(address)
             places = p.query(address)
-            app.logger.info("Logging a test message from {}".format(this_route))
+            app.logger.info("Map request success {}".format(this_route))
             # return those results
             return render_template('map.html', form=form, my_coordinates=my_coordinates, places=places)
 
     elif request.method == 'GET':
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("GET {}".format(this_route))
         return render_template("map.html", form=form, my_coordinates=my_coordinates, places=places)
 
     app.logger.info("Logging a test message from {}".format(this_route))
@@ -74,14 +74,14 @@ def map():
 def signup():
     this_route = url_for('.signup')
     if 'email' in session:
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Already logged in {}".format(this_route))
         return redirect(url_for('home'))
 
     form = SignupForm()
 
     if request.method == "POST":
         if form.validate() == False:
-            app.logger.info("Logging a test message from {}".format(this_route))
+            app.logger.info("Sign up form invalid {}".format(this_route))
             return render_template('signup.html', form=form)
         else:
             newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
@@ -89,25 +89,25 @@ def signup():
             db.session.commit()
 
             session['email'] = newuser.email
-            app.logger.info("Logging a test message from {}".format(this_route))
+            app.logger.info("Sign up successful {}".format(this_route))
             return redirect(url_for('home'))
 
     elif request.method == "GET":
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("GET {}".format(this_route))
         return render_template('signup.html', form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     this_route = url_for('.login')
     if 'email'in session:
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Already logged in {}".format(this_route))
         return redirect(url_for('home'))
 
     form = LoginForm()
 
     if request.method == "POST":
         if form.validate() == False:
-            app.logger.info("Logging a test message from {}".format(this_route))
+            app.logger.info("Log in unsuccessful {}".format(this_route))
             return render_template("login.html", form=form)
         else:
             email = form.email.data
@@ -116,28 +116,28 @@ def login():
             user = User.query.filter_by(email=email).first()
             if user is not None and user.check_password(password):
                 session['email'] = form.email.data
-                app.logger.info("Logging a test message from {}".format(this_route))
+                app.logger.info("Log in successful {}".format(this_route))
                 return redirect(url_for('home'))
             else:
-                app.logger.info("Logging a test message from {}".format(this_route))
+                app.logger.info("Log in unsuccessful {}".format(this_route))
                 return redirect(url_for('login'))
 
     elif request.method == 'GET':
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("GET {}".format(this_route))
         return render_template('login.html', form=form)
 
 @app.route("/logout")
 def logout():
     this_route = url_for('.logout')
     session.pop('email', None)
-    app.logger.info("Logging a test message from {}".format(this_route))
+    app.logger.info("Logged out {}".format(this_route))
     return redirect(url_for('index'))
 
 @app.route("/home/", methods=["GET", "POST"])
 def home():
     this_route = url_for('.home')
     if 'email' not in session:
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Can't visit home page without session key {}".format(this_route))
         return redirect(url_for('login'))
 
     form = SearchForm()
@@ -158,7 +158,7 @@ def home():
         products = p.search_query(pagination, catid, page)
         pages = products.pop(-1)
 
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Viewed pagination {}, {}".format(page, cat_id))
         return render_template('home.html', form=form, products=products, categories=categories, pages=pages, query=pagination, cat_id=catid)
     elif category != None:
             #products = p.search_query("", )
@@ -169,13 +169,13 @@ def home():
                         if category == each['category']:
                             products = p.search_query("", each['cat_id'], 0)
                             pages = products.pop(-1)
-                            app.logger.info("Logging a test message from {}category_select({})".format(this_route, category))
+                            app.logger.info("Category select({})".format(category))
                             return render_template('home.html', form=form, products=products, categories=categories, pages=pages, cat_id=each['cat_id'])
                     else:
                         if category == each['subcat']:
                             products = p.search_query("", each['sub_id'], 0)
                             pages = products.pop(-1)
-                            app.logger.info("Logging a test message from {}category_select({})".format(this_route, category))
+                            app.logger.info("Category select({})".format(category))
                             return render_template('home.html', form=form, products=products, categories=categories, pages=pages, cat_id=each['sub_id'])
                     count += 1
                 count = 0
@@ -205,7 +205,7 @@ def home():
 def product_view():
     this_route = url_for('.product_view')
     if 'email' not in session:
-        app.logger.info("Logging a test message from {}".format(this_route))
+        app.logger.info("Must have session key to view {}".format(this_route))
         return redirect(url_for('login'))
 
     form = SearchForm()
